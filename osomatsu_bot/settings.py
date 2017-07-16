2106a704a89f97366aefa91a5ca04b67c17b107e
albic <albic@sfc.wide.ad.jp>
2017-07-17 04:57:09 +0900

    edit settings.py

--- osomatsu_bot/settings.py
+++ osomatsu_bot/settings.py
@@ -118,3 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'immense-earth-97160.herokuapp.com']
