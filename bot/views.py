dcbfa5a47030e265ca83727c077131c76385268d
albic <albic@sfc.wide.ad.jp>
2017-07-17 13:29:49 +0900

    serif追加

--- bot/views.py
+++ bot/views.py
@@ -6,7 +6,14 @@ import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import sys
+path="/Users/albicilla/programming/osoBOT/osomatsu_bot/bot/"
+sys.path.append(path)
+
+import load_serif
+
 from load_serif import osomatsu_serif  # 先ほどのおそ松のセリフ一覧をimport
+import re #正規表現
 
 REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
 ACCESS_TOKEN = 'cf6MkYQIETJ7+jKqHJxVXDqOjHAGrTNfALgyds2qfY3HIslXGQ7GSAGJpALAa2TAZnLNT6u885N6P6w2BB2Qj1EQpdoiQjut0IVAWBlTOikyJwBbnYeAnRj9po9bwmCTJKH/ciE0bAJ+8PbtAOtERAdB04t89/1O/w1cDnyilFU='
@@ -20,6 +27,13 @@ def index(request):
 
 def reply_text(reply_token, text):
     reply = random.choice(osomatsu_serif)
+
+    r = re.compile("^[a-z0-9]")
+    m = r.search(text)
+    if m == "None":
+        print ("N")
+    else:
+        reply  = str([random.randint(1,100)])
     payload = {
           "replyToken":reply_token,
           "messages":[
--- manage.py
+++ manage.py
@@ -4,6 +4,10 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "osomatsu_bot.settings")
+
+    messe = os.getcwd()
+    print ("LLLLLLLLL")
+    print (messe)
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
