d60a7153757fda529958806c7c18eea049d77978
albic <albic@sfc.wide.ad.jp>
2017-07-25 00:11:40 +0900

    間違って消しちゃった分のブランチのコミット

--- bot/views.py
+++ bot/views.py
@@ -82,6 +82,8 @@ def reply_text(reply_token, text):
         round = str(random.randint(4,14))
         reply = "結果：" + rnum + "\n" + type + "\nラウンド：" + round
     elif re.match(sanc_l, text):
+        rnum = str(random.randint(1,10))
+        type = scl_type[int(rnum)]
         time_list = range(10, 110, 10)
         time_select = random.randint(0,9)
         reply = "結果：" + rnum + "\n" + type + "\n時間：" + str(time_list[time_select])
