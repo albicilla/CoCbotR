7d3df4a182651fc92250f01fab79c2abea2f102e
albic <albic@sfc.wide.ad.jp>
2017-07-28 17:20:30 +0900

    1d100メッセをつけた

--- bot/views.py
+++ bot/views.py
@@ -75,6 +75,7 @@ def reply_text(reply_token, text):
         reply = "【コマンド一覧】\n[数値1]d[数値2]：[数値2]面ダイスを[数値1]回振る\nscs：短期の一時的狂気選択\nscl：長期の一時的狂気選択\n僕は藤岡だ。力になれたら嬉しい。"
     elif m:
         for i in range(int(split_text[0])):
+            reply += split_text[0] + "d" + split_text[1] + "->"
             reply  += str([random.randint(1,int(split_text[1]))])
     elif re.match(sanc_s, text):
         rnum = str(random.randint(1,10))
