db25b88d7b4931c950e3980f23340ecda89ed12b
albic <albic@sfc.wide.ad.jp>
2017-07-29 10:11:41 +0900

    audioテスト修正

--- bot/views.py
+++ bot/views.py
@@ -122,11 +122,11 @@ def reply_text(reply_token, text):
           "replyToken":reply_token,
           "messages":[
                 {
-                    #"type":"text",
-                    #"text": reply
-                    "type":"audio",
-                    "originalContentUrl":"https://www.dropbox.com/s/9v0i83lzj8yrsxh/%E3%81%82%E3%82%8B%E9%9B%A8%E3%81%AE%E6%97%A5.mp3?dl=1",
-                    "duration": 240000
+                    "type":"text",
+                    "text": reply
+                    #"type":"audio",
+                    #"originalContentUrl":"https://www.dropbox.com/s/9v0i83lzj8yrsxh/%E3%81%82%E3%82%8B%E9%9B%A8%E3%81%AE%E6%97%A5.mp3?dl=1",
+                    #"duration": 240000
 
                 }
             ]
